05687943108da465e6e6a5835fb4b1473391d3c1
shyam_tg <shyamsundar.tg@gmail.com>
2020-07-30 00:19:35 +0530

    db chanage

--- gatewayapp/settings.py
+++ gatewayapp/settings.py
@@ -11,10 +11,11 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+import mimetypes
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+mimetypes.add_type("text/css", ".css", True)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
