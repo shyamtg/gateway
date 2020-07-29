d2fc266425cbbc135679e9533beb34ee0d281374
shyam_tg <shyamsundar.tg@gmail.com>
2020-07-29 23:42:08 +0530

    db chanage

--- gatewayapp/settings.py
+++ gatewayapp/settings.py
@@ -75,17 +75,17 @@ WSGI_APPLICATION = 'gatewayapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "gateway",
-        "USER": "root",
-        "PASSWORD": "root123",
-        "HOST": "localhost",
-        "PORT": "3306",
-    }
-}
 
+DATABASES = {
+     'default': {
+         'ENGINE': 'django.db.backends.mysql',
+         'NAME': 'gateway',
+         'USER': 'admin',
+         'PASSWORD': 'Root@123',
+         'HOST': 'localhost',
+         'PORT': '3306'
+     }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
